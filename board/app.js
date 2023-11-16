b93d36531faf79341ad04b7436b02cb18eb9dc63
Jeseop Hong <ds0794@naver.com>
2023-11-16 11:07:04 +0900

    리스트 API 만들기

--- board/app.js
+++ board/app.js
@@ -16,11 +16,16 @@ app.engine(
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    title: "테스트 게시판",
-    message: "만나서 반갑습니다!",
-  });
+app.get("/", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const search = req.query.search || "";
+  try {
+    const [posts, paginator] = await postService.list(collection, page, search);
+    res.render("home", { title: "테스트 게시판", search, paginator, posts });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { title: "테스트 게시판" });
+  }
 });
 
 app.get("/write", (req, res) => {
